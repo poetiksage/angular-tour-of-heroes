16279979c882f611c43b424585300888ce676825
Nilesh Fourwit <nilesh12.fourwit@gmail.com>
2023-10-04 10:05:38 +0530

    working on Add services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,10 +9,16 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero
+  }
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
