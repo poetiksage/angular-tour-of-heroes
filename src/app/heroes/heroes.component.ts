09e9058fbf6d16bda6e89572abdb57ba1e954d75
Nilesh Fourwit <nilesh12.fourwit@gmail.com>
2023-10-11 10:39:02 +0530

    working on navigation and solving errors

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -18,11 +18,6 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {
     this.getHeroes();
-  }  
-  
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes():void {
